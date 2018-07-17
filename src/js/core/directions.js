708d9f254df5f5736f9514560fada0509cffc2d1
Tim Johnson <tjohnson@scitent.com>
2018-07-17 09:40:29 -0400

    added names for logging clarity

--- src/js/core/directions.js
+++ src/js/core/directions.js
@@ -1,6 +1,6 @@
 export default {
-  north: { x: 0, y:-1 },
-  south: { x: 0, y:1 },
-  east: { x: 1, y:0 },
-  west: { x: -1, y:0 }
-}
+  north: { x: 0,   y:-1, name: 'north' },
+  south: { x: 0,   y:1,  name: 'south' },
+  east:  { x: 1,   y:0,  name: 'east'  },
+  west:  { x: -1,  y:0,  name: 'west'  }
+};
