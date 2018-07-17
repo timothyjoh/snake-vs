6a1a1e8cfaa5098d790886a49b137d7810a093d0
Tim Johnson <tjohnson@scitent.com>
2018-07-17 09:39:41 -0400

    next just passes through the state and returns a new one

---
+++ src/js/engine/next.js
@@ -0,0 +1,26 @@
+import d from '../core/directions';
+import nextMoves from './next_move';
+import nextSnake from './next_snake';
+import nextApple from './next_apple';
+
+const initialState = {
+  cols:  20,
+  rows:  14,
+  moves: [d.east],
+  snake: [{x: 2, y: 2}, {x: 3, y: 2}],
+  apple: { x: 16, y: 2 },
+};
+
+const next = state => {
+  const newstate = {
+    rows:  state.rows,
+    cols:  state.cols,
+    moves: nextMoves(state),
+    snake: nextSnake(state),
+    apple: nextApple(state)
+  };
+  // console.log('next', state, newstate);
+  return newstate;
+};
+
+export {next, initialState};
